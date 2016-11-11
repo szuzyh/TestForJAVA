5e6ce038086c9eafd186895d67afebc7a58bc0d7
aceszuzyh <szuzyh@126.com>
2016-11-11 17:11:03 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,72 @@
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
+import spark.Spark;
+
+import static spark.route.HttpMethod.get;
+
+/**
+ * Created by Leo on 2016/11/11.
+ */
+
+public class Main {
+
+    private static ActiveXComponent com;
+    private static Dispatch disp;
+    public static void main(String[] args){
+        com = new ActiveXComponent("CLSID:0F55CC69-97EF-42A9-B63D-D1831CB2B3B9");
+        disp = (Dispatch)com.getObject();
+        int ret = Dispatch.call(disp, "getCardInfo", new Variant("E:\\Img\\head.jpg")).getInt();
+        if(ret!=0){
+
+//            System.out.print(Dispatch.call(disp, "Sex").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "Nation").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "Birthday").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "Address").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "ID").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "Department").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "StartDate").getString()+"\n");
+//            System.out.print(Dispatch.call(disp, "EndDate").getString()+"\n");
+            System.out.println("打开设备失败！");
+        }
+        //获取各项单项信息
+        getMessage("Name");
+        getMessage("Sex");
+        getMessage("Nation");
+        getMessage("Birthday");
+        getMessage("Address");
+        getMessage("ID");
+        getMessage("Department");
+        getMessage("StartDate");
+        getMessage("EndDate");
+
+        //获取全部信息并打印成表格xml
+        getAllMessageWithoutID();
+
+    }
+
+    private static void getAllMessageWithoutID() {
+
+        Spark.get("/message",(request, response) -> {
+            String name=Dispatch.call(disp,"Name").getString().trim();
+            String sex=Dispatch.call(disp,"Sex").getString().trim();
+            String nation=Dispatch.call(disp,"Nation").getString().trim();
+            String birthday=Dispatch.call(disp,"Birthday").getString().trim();
+            String address=Dispatch.call(disp,"Address").getString().trim();
+            String ID=Dispatch.call(disp,"ID").getString().trim();
+            String department=Dispatch.call(disp,"Department").getString().trim();
+            String startDate=Dispatch.call(disp,"StartDate").getString().trim();
+            String endDate=Dispatch.call(disp,"EndDate").getString().trim();
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><news>"+"姓名："+name
+                    +"性别："+sex+"</news>";
+        });
+    }
+
+    private static void getMessage(String s) {
+        Spark.get("/message/"+s,(request, response) -> {
+            return Dispatch.call(disp,s).getString().trim();
+        });
+    }
+
+
+}
